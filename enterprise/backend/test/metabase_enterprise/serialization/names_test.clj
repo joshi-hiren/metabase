84e226422b809a75abf921eaa3c24c6471e45fe9
Jeff Evans <jeff.evans@metabase.com>
2021-05-12 17:07:28 -0500

    Serialization fixes: support native query snippets
    Adding support for NativeQuerySnippet:
    
    - in names
    - in dump (putting into subdirectory of collection, as cards)
    - in upsert (identity condition of :name :collection_id)
    - in load (no retry fn necessary since they should not have any dependencies themselves)
    
    Adding a snippet and a card referencing it to roundtrip test (including a deeply nested snippet)
    
    Fixing up handling of snippet namespaced collections and adding relevant name tests

--- enterprise/backend/test/metabase_enterprise/serialization/names_test.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/names_test.clj
@@ -2,8 +2,9 @@
   (:require [clojure.test :refer :all]
             [metabase-enterprise.serialization.names :as names]
             [metabase-enterprise.serialization.test-util :as ts]
-            [metabase.models :refer [Card Collection Dashboard Database Field Metric Segment Table]]
-            [metabase.util :as u]))
+            [metabase.models :refer [Card Collection Dashboard Database Field Metric NativeQuerySnippet Segment Table]]
+            [metabase.util :as u]
+            [metabase.test :as mt]))
 
 (deftest safe-name-test
   (are [s expected] (= (names/safe-name {:name s}) expected)
@@ -32,8 +33,39 @@
                     (Collection collection-id)
                     (Collection collection-id-nested)
                     (Dashboard dashboard-id)
-                    (Database db-id)]]
+                    (Database db-id)
+                    (NativeQuerySnippet snippet-id)]]
       (testing (class object)
-        (let [context (names/fully-qualified-name->context (names/fully-qualified-name object))]
+        (let [context (names/fully-qualified-name->context (names/fully-qualified-name object))
+              id-fn   (some-fn :snippet :field :metric :segment :card :dashboard :collection :table :database)]
           (is (= (u/the-id object)
-                 ((some-fn :field :metric :segment :card :dashboard :collection :table :database) context))))))))
+                 (id-fn context))))))))
+
+(deftest fully-qualified-name->context-test
+  (testing "fully-qualified-name->context works as expected"
+    (testing "with cards in root and in a collection"
+      (mt/with-temp* [Collection [{collection-id :id} {:name "A Collection"}]
+                      Card       [root-card {:name "Root Card"}]
+                      Card       [collection-card {:name         "Collection Card"
+                                                   :collection_id collection-id}]]
+        (let [card1-name "/collections/root/cards/Root Card"
+              card2-name "/collections/root/collections/A Collection/cards/Collection Card"]
+          (is (= card1-name (names/fully-qualified-name root-card)))
+          (is (= card2-name (names/fully-qualified-name collection-card))))))
+    (testing "with snippets in a collection"
+      (mt/with-temp* [Collection [{base-collection-id :id} {:name "Base Collection"
+                                                            :namespace "snippets"}]
+                      Collection [{collection-id :id}      {:name "Nested Collection"
+                                                            :location (format "/%s/" base-collection-id)
+                                                            :namespace "snippets"}]
+                      NativeQuerySnippet [snippet {:content "price > 2"
+                                                   :name "Price > 2"
+                                                   :description "Price more than 2"
+                                                   :collection_id collection-id}]]
+         (let [fully-qualified-name (str "/collections/root/collections/:snippets/Base Collection/collections"
+                                         "/:snippets/Nested Collection/snippets/Price %3E 2")]
+           (is (= fully-qualified-name
+                  (names/fully-qualified-name snippet)))
+           (is (= {:collection collection-id
+                   :snippet    (u/the-id snippet)}
+                  (names/fully-qualified-name->context fully-qualified-name))))))))
